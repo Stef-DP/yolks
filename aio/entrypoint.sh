befd483c748cba2f2caad437681e3603ad99fbb7
Stef-00012 <me@stefdp.com>
2026-07-26 12:10:20 +0200

    update entrypoint

--- aio/entrypoint.sh
+++ aio/entrypoint.sh
@@ -2,8 +2,8 @@
 cd /home/container
 
 # Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo -e ":/home/container$ ${MODIFIED_STARTUP}"
+MODIFIED_STARTUP=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-${MODIFIED_STARTUP}
+exec /bin/bash -c "${MODIFIED_STARTUP}"
